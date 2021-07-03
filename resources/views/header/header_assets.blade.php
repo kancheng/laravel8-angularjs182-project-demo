b788e2b522ed0919bbf6664ba5333a5ed46264bd
kancheng <zxdfgcv@gmail.com>
2021-07-03 20:31:45 +0800

    Edited

--- resources/views/header/header_assets.blade.php
+++ resources/views/header/header_assets.blade.php
@@ -1,5 +1,5 @@
 <!-- Header CSS -->
-<link rel="stylesheet" type="text/css" href="{{ asset('public/assets/css/main.css') }}">
+<link rel="stylesheet" type="text/css" href="/assets/css/main.css">
 <style type="text/css">
 	/* CSS */
 </style>
